6b392412c9fb9d3da6f8195e7a2fa3d23a0e113c
Apurv Puri <apurv.puri@gmail.com>
2013-12-08 22:54:08 -0600

    Sections 1.3 solved. Section 1.2 is still remaining.

---
+++ Chapter1/motif.py
@@ -0,0 +1,27 @@
+import re
+
+
+def subs(handle):
+    """
+    (str) -> None
+
+    Function subs prints the locations of the motif specified in the DNA in question
+    """
+    f = open(handle, 'r')
+
+    motif = "CTTGATCAT"
+    dna = f.readline()
+
+    # Remove any trailing whitespace
+    dna = dna.rstrip()
+    motif = motif.rstrip()
+
+    # Use a list comprehension using Reg Exps
+    # Use a look ahead assertion when using Reg Exp
+
+    forward = '(?='
+    back = ')'
+    patt = forward + motif + back
+    zeroindex = [m.start() for m in re.finditer(patt, dna)]
+
+    print(' '.join(map(str, zeroindex)))
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Stepic Code',
+    version='',
+    packages=['Chapter1', 'Chapter2'],
+    url='',
+    license='',
+    author='Apurv',
+    author_email='apurv.puri@gmail.com',
+    description=''
+)
