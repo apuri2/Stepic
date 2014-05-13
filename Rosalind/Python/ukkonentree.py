ab119a3f84225a29d8f70479695d861f5760f068
apuri <apurv.puri@gmail.com>
2014-05-12 19:01:29 -0500

    Function fastaread added.

---
+++ Rosalind/Python/ukkonentree.py
@@ -0,0 +1,14 @@
+__author__ = 'apuri'
+
+
+class SuffixTree(object):
+
+    class Node(object):
+        pass
+
+    class Edge(object):
+        pass
+
+    def __init__(self):
+        # Construct the base suffix tree for a string
+        pass
