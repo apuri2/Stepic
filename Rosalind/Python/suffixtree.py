e65f8cf0bc03ca465219ca2bff9c3ce221423068
apuri <apurv.puri@gmail.com>
2014-12-12 18:36:42 -0600

    Version 0.3 Building the Suffix Tree:     - Core method is being added     - In the process of translating pseudo code into working python code
        TO DO:
         - Unit tests for smaller independent objects: Edge, Node, etc
         - Naive implementation in O(m^2) time for suffix trees

--- Rosalind/Python/suffixtree.py
+++ Rosalind/Python/suffixtree.py
@@ -1,41 +1,54 @@
-__author__ = 'apuri'
+__author__ = 'Apurv Puri'
+
 
 class UkkonenTree(object):
     """
     Uses Ukkonen's algorithm to create a suffix tree in linear time.
+    Suffix Tree is a collection of Edges and Nodes to
     """
 
-    # Constructor
-    def __init__(self, string):
-        self.string = string
+    def __init__(self, genome):
+        self.genome = genome
+        self.nodes = [Node()]       # Collection of nodes in tree, also initializes the root node
+        self.edges = {}             # Collection of edges in tree
 
         # Build tree
+        for index, char in enumerate(genome):
+            self._add_Prefix(index)
+
+
+    def _add_Prefix(self, index):
+        """
+        Main method to create the suffix tree.
+        """
+        # Always start at the root node?
+        last_node_index = -1
         while True:
-            if not string:
-                break
+            edge = Edge()
 
-    # Public Methods
-    def longComSubString(self, refString):
-        """
-        Returns the longest common substring of an arbitrarily large string
-        str -> str
-        @return:
-        """
-        pass
-
-class Edge(self, firstChar, lastChar):
+class Edge(object):
     """
     Edge class. Contains pointers to the first and last character of string
     """
-    self.firstChar = firstChar
-    self.lastChar = lastChar
+    def __init__(self, first_nuc_index, last_nuc_index, from_node, to_node):
+        """
+        """
+        self.first_nuc_index = first_nuc_index
+        self.last_nuc_index = last_nuc_index
+        self.from_node = from_node
+        self.to_node = to_node
 
     @property
-    def __init__(self):
-        assert isinstance(self.lastChar, object)
-        return[self.firstChar, self.lastChar]
+    def length(self):
+        return self.last_nuc_index - self.first_nuc_index
 
-class Node(self, nodeId=default):
+
+class Node(object):
     """
-    Node class.
+    Node class
     """
+    def __init__(self, suffix_node=-1):
+        self.suffix_node = suffix_node
+
+    def is_root(self):
+        return self.suffix_node == -1
